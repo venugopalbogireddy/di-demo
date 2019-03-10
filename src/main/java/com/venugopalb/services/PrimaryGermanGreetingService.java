6e5f112c4294ee067fff47c34d0e7ec031fb45e6
venugopalbogireddy <venugopal.bogireddy@stonybrook.edu>
2019-03-10 18:50:47 -0400

    Dependency Injection

--- src/main/java/com/venugopalb/services/PrimaryGermanGreetingService.java
+++ src/main/java/com/venugopalb/services/PrimaryGermanGreetingService.java
@@ -1,11 +1,15 @@
 package com.venugopalb.services;
 
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
 /**
  * Created by jt on 5/24/17.
  */
-//@Service
-//@Primary
-//@Profile("de")
+@Service
+@Primary
+@Profile("de")
 public class PrimaryGermanGreetingService implements GreetingService {
 
     private GreetingRepository greetingRepository;
